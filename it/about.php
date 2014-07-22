56b7fc571d9982881f9a80a5085d74ba4de8cce3
STEVE <slear@email.it>
2014-07-22 14:56:25 +0200

    utf-8 e typo

--- it/about.php
+++ it/about.php
@@ -13,6 +13,6 @@ $translations = array(
 
     'p2' => 'Lo scopo del nostro progetto è quello di soddisfare le necessità degli sviluppatori di testare la <b>versione corrente</b> di MySQL. Poichè aggiorniamo le versioni di MySQL e phpMyAdmin frequentemente, avrete la possibilità di testare il vostro sito Web con la versione corrente di MySQL.',
 
-    'h3' => '<a href="signup.php">Ottieni il tou account gratuito per gestire un database MySQL&nbsp;&raquo;</a>',
+    'h3' => '<a href="signup.php">Ottieni il tuo account gratuito per gestire un database MySQL&nbsp;&raquo;</a>',
 );
 ?>
