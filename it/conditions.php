214cd9eae393e162b2588d9275750ad0ce77ea23
STEVE <slear@email.it>
2014-07-22 15:11:51 +0200

    First serious page

--- it/conditions.php
+++ it/conditions.php
@@ -7,19 +7,19 @@ Language: Italian
 $translations = array(
     'h1' => 'Condizioni d'uso',
 
-    'p1' => 'In no event shall we, the db4free.net team, be liable to any party for direct, indirect, special, incidental, or consequential damages, including lost profits, arising out of the use of or the unavailability of this host, its software, its services or its documentation even if the administrator has been advised of the possibility of such damage.',
+    'p1' => 'In nessun caso noi, il team di sviluppo di db4free.net, potremo essere considerati responsabili in qualsiasi misura per danni diretti, indiretti, speciali, incidentali o consequenziali, includendo anche perdita di profitti, derivanti dall'uso o dalla indisponibilità di questo server, del suo software, dei suoi servizi e della sua documentazione anche se l'amministratore di questo server viene avvisato della possibilità che si verifichino questi danni.',
 
-    'p2' => 'We specifically disclaim any warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose. The software and host provided hereunder is on an "as is" basis, and we have no obligations to provide maintenance, support, updates, enhancements, or modifications or continued availability of services. We are entitled to discontinue this service without notification and delete the databases irrevocably.',
+    'p2' => 'Specificatamente noi non garantiamo, includendo e non limitandoci, implicite garanzie di utilizzabilità o adeguatezza per qualsiasi particolare proposito. Il software ed il server è qui sulla base di "così com'è", e non abbiamo alcun obbligo di fornire manutenzione, supporto, aggiornamenti, miglioramenti o modifiche o continua disponibilità dei servizi. Siamo autorizzati a dismettere questo servizio senza notifica e possiamo cancellare i databases irrevocabilmente',
 
-    'p3' => 'Users of db4free.net are not permitted to publish their access data as well as server specific data to third parties.',
+    'p3' => 'Gli utenti di db4free.net non possono pubblicare i loro dati di accesso come pure dati specifici sul server a terze parti.',
 
-    'p4' => 'Exclusively the laws of the Republic of Austria apply to services relating to the db4free.net project.',
+    'p4' => 'Le leggi della Repubblica Austriaca solo le uniche che possono essere applicate al progetto db4free.net.',
 
-    'p5' => 'We do not recommend you to use a db4free.net database for any critical data and for any critical purpose! Even though we - the people behind this project - are related to each other through a company, this is still a private and non commercial project. We refuse any warranty and liability!',
+    'p5' => 'Non raccomandiamo di utilizzare un database ospitato da db4free.net per qualsiasi dato critico e per qualsiasi attività critica! Anche se noi, - le persone che hanno creato questo progetto - siamo collegato l'un l'altro attraverso una società, questo è comunque un progetto privato e non commerciale. Non forniamo nessuna garanzia e non accettiamo nessuna responsabilità',
 
-    'p6' => 'The db4free.net Team does not take any liability for the contents of linked web pages.',
+    'p6' => 'La squadra di sviluppo di db4free.net non si assume nessuna responsabilità per i contenuti delle pagine web collegate a questo sito.',
 
-    'p7' => 'We retain the right to modify this statement without notice.',
+    'p7' => 'Manteniamo il diritto di modificare queste condizioni senza preavviso.',
 
     'p8' => 'Heidenreichstein, Austria<br>29 June 2014',
 );
