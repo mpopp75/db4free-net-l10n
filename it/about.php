4b2d0356ca59085ad76d2f3d7215af9d3071a814
STEVE <slear@email.it>
2014-07-22 14:53:01 +0200

    UTF-8

--- it/about.php
+++ it/about.php
@@ -7,11 +7,11 @@ Language: Italian
 $translations = array(
     'h1' => 'Informazioni sul progetto db4free.net',
 
-    'p1' => 'In questa sezione Ë possibile creare il vostro database, modificare i suoi dati e cancellarlo.',
+    'p1' => 'In questa sezione √® possibile creare il vostro database, modificare i suoi dati e cancellarlo.',
 
     'h2' => 'Che cosa ci rende "speciali"?',
 
-    'p2' => 'Lo scopo del nostro progetto Ë quello di soddisfare le necessit‡ degli sviluppatori di testare la <b>versione corrente</b> di MySQL. PoichË aggiorniamo le versioni di MySQL e phpMyAdmin frequentemente, avrete la possibilit‡ di testare il vostro sito Web con la versione corrente di MySQL.',
+    'p2' => 'Lo scopo del nostro progetto √® quello di soddisfare le necessit√† degli sviluppatori di testare la <b>versione corrente</b> di MySQL. Poich√® aggiorniamo le versioni di MySQL e phpMyAdmin frequentemente, avrete la possibilit√† di testare il vostro sito Web con la versione corrente di MySQL.',
 
     'h3' => '<a href="signup.php">Ottieni il tou account gratuito per gestire un database MySQL&nbsp;&raquo;</a>',
 );
