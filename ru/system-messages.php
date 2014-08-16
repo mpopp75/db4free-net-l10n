74fb6fa0e31034c73dbad0981b42743ea5db4357
bundle install gems <cyber-01@yandex.ru>
2014-08-16 05:40:54 +0400

    Added Russian translation

---
+++ ru/system-messages.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Section: system-messages
+Language: Russian
+*/
+
+$translations = array(
+    'database' => 'База данных',
+
+    'username' => 'Username',
+
+    'email' => 'E-mail',
+
+    'id-not-found' => 'ID не найден.',
+
+    'success-create-db' => 'База данных успешно создана.',
+
+    'error-create-db' => 'Ошибка создания базы данных.',
+
+    'success-data-change' => 'Данные изменены.',
+
+    'error-data-change' => 'Ошибка при изменении данных.',
+
+    'success-account-delete' => 'Аккаунт был удален.',
+
+    'error-account-delete' => 'Ошибка при удалении аккаунта.',
+
+    'another-user-database' => 'Имя базы данных уже используется другим пользователем: ',
+
+    'another-user-username' => 'Имя пользователя уже используется другим пользователем: ',
+
+    'passwords-not-identical' => 'Пароли не совпадают.',
+
+    'password-too-short' => 'Пароль должен содержать минимум 6 символов.',
+
+    'only-2-db-allowed' => 'На один E-mail разрешено только 2 базы данных.',
+
+    'email-invalid' => 'E-mail неверен.',
+
+    'user-not-exist' => 'Пользователь не существует.',
+
+    'user-not-confirmed' => 'Пользователь не подтвердил свой E-mail.',
+
+    'password-not-correct' => 'Пароль неверен.',
+
+    'confirmation-email' => 'Вам отправлено E-mail сообщение, для подтверждения изменений.',
+
+    'error-write-data' => 'Произошла ошибка при обновлении ваших данных.',
+
+    'error-send-email' => 'Произошла ошибка при подтверждении вашего E-mail.',
+
+    'subject-signup' => 'Регистрация вашей базы данных на db4free.net',
+
+    'subject-delete-account' => 'Удаление вашего аккаунта с db4free.net',
+
+    'subject-change-password' => 'Ваш новый пароль на db4free.net',
+);
+?>
