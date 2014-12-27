8051bf1a6248ceb0c079cd7d51da721c1d233d37
Jonathan Herlin <jonte@jherlin.se>
2014-12-27 13:34:10 +0100

    Fixed minor errors in welcome.php

--- sv/welcome.php
+++ sv/welcome.php
@@ -7,13 +7,13 @@ Language: Swedish
 $translations = array(
     'h1' => 'Välkommen till db4free.net',
 
-    'p1a' => 'db4free.net tillhandahåller en tjänst där du kan testa - ibland även utveckla mot - nya versioner av <a href="http://www.mysql.com/">MySQL Server</a>. Du kan enkelt <a href="/signup.php">skapa ett konto gratis</a> och testa dina applikationer, till exempel att se till att de fortfarande fungerar efter en MySQL version uppdatering. db4free.net är också en bra resurs för utbildning och för att göra dig bekant med nya funktioner som introduceras i nya versioner.',
+    'p1a' => 'db4free.net tillhandahåller en tjänst där du kan testa - ibland även utveckla mot nya versioner av <a href="http://www.mysql.com/">MySQL Server</a>. Du kan enkelt <a href="/signup.php">skapa ett konto gratis</a> och testa dina applikationer för att t.ex. se till att de fortfarande fungerar efter en  uppdatering av MySQL. db4free.net är också en bra resurs för utbildning och för att göra dig bekant med funktioner som introduceras i nya versioner.',
 
     'p1b' => 'db4free.net syftar att alltid ge antingen den senaste produktionsversionen eller den senaste utvecklingsversionen. db4free.net\'s MySQL server kommer att uppdateras mycket snart efter en ny version släpps, oftast samma dag eller mycket snart efter.',
 
     'p1c' => 'För att komma åt din data på ett bekvämt sätt så tillhandahåller db4free.net också en up-to-date version av <a href="http://www.phpmyadmin.net"> phpMyAdmin </a>. phpMyAdmin kommer också att uppdateras mycket ofta så att du alltid får det allra senaste versionen att jobba med.',
 
-    'h2' => 'vad db4free.net inte är',
+    'h2' => 'Vad db4free.net inte är',
 
     'p2' => 'db4free.net är en <b>testtjänst</b> vilket betyder att det inte är lämpligt för produktion. Det kan finnas avbrott, förlust av data och säkerhetsfunktioner uppfyller inte de standarder som du förväntar dig av en professionell databasleverantör. Om du behöver en MySQL-databas för produktionsanvändning så ska du inte använda db4free.net!',
 
@@ -23,9 +23,9 @@ $translations = array(
 
     'h4' => 'Resurser',
 
-    'p4a' => 'Du hittar nyheter gällande db4free.net på denna blog under kategorin "db4free.net" <a href="http://www.mpopp.net/category/db4free/">db4free.net section</a> på <a href="http://www.mpopp.net/">mpopp.net blog</a> här hittar du alltid de senaste uppdatteringarna gällande db4free.net. Vänligen prenumerera på <a href="http://www.mpopp.net/category/db4free/feed/">RSS Feed</a> att se till att du inte missar några nyheter. db4free.net finns också på <a href="/twitter.php">Twitter</a>, för att vara på toppen av vad som händer i db4free.net värld.',
+    'p4a' => 'Du kan läsa alla nyheter gällande db4free.net på <a href="http://www.mpopp.net/category/db4free/">db4free.net sektionen</a> på <a href="http://www.mpopp.net/">mpopp.net blogg</a> här hittar du alltid de senaste uppdatteringarna gällande db4free.net. Du kan öven prenumerera på <a href="http://www.mpopp.net/category/db4free/feed/">RSS Flödet</a> för att se till att du inte missar några nyheter. db4free.net finns också på <a href="/twitter.php">Twitter</a> så att du aldrig missar vad som händer på db4free.net',
 
-    'p4b' => 'De bästa resurserna för att lära sig mer om MySQL är <a href="http://dev.mysql.com/">MySQL Developer Zone</a>, <a href="http://dev.mysql.com/doc/refman/5.6/en/index.html">MySQL Reference Manual</a> och <a href="http://planet.mysql.com/">PlanetMySQL</a>. MySQL hemsida erbjuder ett antal <a href="http://dev.mysql.com/tech-resources/articles/">Artiklar för utvecklare</a> varav många förklarar nya funktioner som introduceras i kommande versioner i minsta lilla detalj.',
+    'p4b' => 'De bästa resurserna för att lära sig mer om MySQL är <a href="http://dev.mysql.com/">MySQL Avdelning för utvecklare</a>, <a href="http://dev.mysql.com/doc/refman/5.6/en/index.html">MySQL Reference Manual</a> och <a href="http://planet.mysql.com/">PlanetMySQL</a>. MySQL hemsida erbjuder ett antal <a href="http://dev.mysql.com/tech-resources/articles/">Artiklar för utvecklare</a> varav många beskriver och förklarar nya funktioner som introduceras i kommande versioner i minsta lilla detalj.',
 
     'p4c' => 'Om du hittar en bugg i MySQL Server, vänligen rapportera det i <a href="http://bugs.mysql.com/">MySQL Bug Tracking System</a>.',
 );
