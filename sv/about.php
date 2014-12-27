82f9fd840c410e2bdfc8d89fc41f72b70c79a90a
Jonathan Herlin <jonte@jherlin.se>
2014-12-27 13:44:22 +0100

    typos and language

--- sv/about.php
+++ sv/about.php
@@ -9,10 +9,10 @@ $translations = array(
 
     'p1' => 'I denna sektion kan du hantera dina databaskonton, modifiera innehåll och radera innehåll.', 
 
-    'h2' => 'Vad får oss att vara "speciella?"?',
+    'h2' => 'Vad får oss att vara "speciella"?',
 
-    'p2' => 'Syftet med vårt projekt är att vi avser att tillgodose behoven hos utvecklare och testare för <b> aktuella versioner </ b> av MySQL. Genom att uppdatera versioner av MySQL och phpMyAdmin ofta, har du till exempel möjlighet att testa din webbplats med en aktuell MySQL-version.',
+    'p2' => 'Syftet med vårt projekt är att tillgodose behoven hos utvecklare och testare för <b> aktuella versioner </b> av MySQL. Genom att uppdatera versioner av MySQL och phpMyAdmin ofta, har du till exempel möjlighet att testa din webbplats med en aktuell MySQL-version innan du gör den skarpa uppdateringen på ditt system.',
 
-    'h3' => '<a href="signup.php">Få din egen gratis MySQL-databas konto&nbsp;&raquo;</a>',
+    'h3' => '<a href="signup.php">Skapa din egna MySQL konto helt gratis &nbsp;&raquo;</a>',
 );
 ?>
