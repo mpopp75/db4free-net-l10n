89caed2bf104d6b7c4f01e4eb08df86ca3f4d39b
duhsoares21 <eduardo@pladoo.com.br>
2015-02-06 16:56:21 -0200

    Tradução db4free.net
    Complete pt-BR(brazilian portuguese) translation

---
+++ pt-BR/welcome.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Section: welcome
+Language: English
+*/
+
+$translations = array(
+    'h1' => 'Bem-vindo ao db4free.net',
+
+	'p1a' => 'O db4free.net fornece um serviço para teste com a mais recente - as vezes versões em desenvolvimento - do <a href="http://www.mysql.com/">MySQL Server</a>. Você pode facilmente <a href="/signup.php">criar uma conta grátis</a> e teste suas aplicações, por exemplo para ter certeza que seus sistemas continuarão funcionando após a atualização do MySQL. O db4free.net também serve como um bom recurso de aprendizado e para ajudá-lo a se familiarizar com as novidades introduzidas em novas versões.',
+
+    'p1b' => 'O db4free.net visa sempre trazer a ultima versão final ou a última versão em desenvolvimento do MySQL server. O MySQL server do db4free.net será atualizado pouco tempo depois de uma nova versão do MySQL ser liberada, normalmente no mesmo dia ou pouco depois.',
+
+    'p1c' => 'Para acessar os seus dados de uma maneira mais conveniente, o db4free.net também disponibiliza uma versão atualizada do <a href="http://www.phpmyadmin.net">phpMyAdmin</a>. O phpMyAdmin também será atualizado frequentemente, para que você sempre tenha a versão mais atual.',
+
+    'h2' => 'O que o db4free.net não é',
+
+    'p2' => 'db4free.net é um <b>serviço de testes</b> o que siginifica que não é feito para produtos finais. Podem haver quedas de energia, perda de dados e não haverá funcionalidades de segurança esperadas em serviços de hospedagem. Se você precisa de um servidor MySQL para produto final, não use o db4free.net!',
+
+    'h3' => 'Você pode ajudar a traduzir o db4free.net?',
+
+    'p3' => 'Você fala bem um idioma que esse site não disponibiliza? Você pode ajudar a traduzir o db4free.net. <a href="/translate.php">Descubra como!</a>',
+
+    'h4' => 'Recursos',
+
+    'p4a' => 'Existe uma <a href="http://www.mpopp.net/category/db4free/">seção db4free.net</a> no <a href="http://www.mpopp.net/">blog mpopp.net</a> que trás as novidades do db4free.net. Por favor, assine nosso <a href="http://www.mpopp.net/category/db4free/feed/">Feed RSS</a> e não perca nenhuma novidade. db4free.net também está no <a href="/twitter.php">Twitter</a>, mais um recurso para te manter atualizado sobre o que está acontecendo no mundo db4free.net.',
+
+    'p4b' => 'O melhor recurso para aprender sobre MySQL é <a href="http://dev.mysql.com/">MySQL Developer Zone(link em inglês)</a>, o <a href="http://dev.mysql.com/doc/refman/5.6/en/index.html">MySQL Reference Manual(link em inglês)</a> e o <a href="http://planet.mysql.com/">PlanetMySQL</a>. O site do MySQL oferece inumeros <a href="http://dev.mysql.com/tech-resources/articles/">Artigos para desenvolvedores(link em inglês)</a> Muitos deles explicando detalhadamente as novas funcionalidades introduzidas em versões recentes do MySQL.',
+
+    'p4c' => 'Se você encontrar um bug no MySQL server, reporte para o <a href="http://bugs.mysql.com/">MySQL Bug Tracking System</a>.',
+);
+?>
