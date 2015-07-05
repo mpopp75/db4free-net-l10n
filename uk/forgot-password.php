a0b7cb0423e2b2a4e4382f44678a65ba9eec672c
shoomilka <shoomilka@i.ua>
2015-07-05 22:02:50 +0300

    Create forgot-password.php

---
+++ uk/forgot-password.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Section: forgot-password
+Language: Ukrainian
+*/
+
+$translations = array(
+    'h1' => 'Забули пароль?',
+
+    'username' => 'Ім\'я користувача',
+
+    'request-new-password' => 'Змінити пароль',
+
+    'ph1' => 'Введіть ім\'я користувача',
+
+    'error1' => 'Введені вами дані невірні!',
+
+    'success' => 'Новий пароль відправлено на ваш E-mail!',
+);
+?>
