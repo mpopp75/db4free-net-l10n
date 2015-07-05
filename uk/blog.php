21f98b103d477eecc46d29207e28db550a7fcc93
shoomilka <shoomilka@i.ua>
2015-07-05 21:55:20 +0300

    Create blog.php

---
+++ uk/blog.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Section: blog
+Language: Ukrainian
+*/
+
+$translations = array(
+    'h1' => 'Блог mpopp.net',
+
+    'visitblog' => 'Відвідати блог mpopp.net (відкриється у новій вкладці)',
+
+    'listposts' => 'Перелік статей блога:',
+);
+?>
