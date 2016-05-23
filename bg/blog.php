d259bb69a8ac3bb4a9ba30c91843d91df461b41c
Yordan <yordan.nedelchev@hotmail.com>
2016-05-23 16:51:17 +0300

    Introducing new translation to Bulgarian.

---
+++ bg/blog.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Section: blog
+Language: English
+*/
+
+$translations = array(
+    'h1' => 'Блог mpopp.net',
+
+    'visitblog' => 'Посетете сайта на блога mpopp.net  (ще се отвори в нов таб на браузъра)',
+
+    'listposts' => 'Списък на статии в блога:',
+);
+?>
