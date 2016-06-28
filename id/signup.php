28a5b40c9bcaefbfd072ccf77e3c9e4020225726
lowsprofile <zdumb1885@outlook.com>
2016-06-28 12:02:31 +0700

    Translation for Bahasa Indonesia added

---
+++ id/signup.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Section: signup
+Language: English
+*/
+
+$translations = array(
+    'h1' => 'Daftar',
+
+    'mysql-db-name' => 'Nama database MySQL',
+
+    'mysql-user-name' => 'Nama pengguna MySQL',
+
+    'mysql-user-password' => 'Kata sandi pengguna MySQL',
+
+    'mysql-user-password-verification' => 'Verifikasi kata sandi pengguna MySQL',
+
+    'email-address' => 'Alamat Email (Surel)',
+
+    'agree-conditions' => 'Saya telah membaca <a href="conditions.php">syarat penggunaan</a> dan saya setuju.',
+
+    'ph1' => '4-16 karakter., tidak dengan huruf besar, pertama haruslah karakter.',
+
+    'ph2' => 'Minimal 6 karakter.',
+
+    'ph3' => 'Masukkan alamat email Anda',
+
+    'explanation' => 'Nama pengguna dan nama database haruslah huruf kecil, angka dan juga garis bawah dan juga harus 4 sampai 16 karakter. Anda tidak boleh menggunakan <a href="https://dev.mysql.com/doc/refman/5.7/en/reserved-words.html"><em>reserved words</em></a>!',
+
+    'maintenance-notice' => 'Due to database maintenance, no registrations are currently possible.',
+
+    'agm-p1' => 'By registering for a db4free.net account you agree that:',
+
+    'agm-li1' => 'db4free.net is a testing environment',
+
+    'agm-li2' => 'db4free.net is not suitable for production',
+
+    'agm-li3' => 'if you decide to use your db4free.net database in production despite the warnings, you do that at your own risk (very frequent backups are highly recommended)',
+
+    'agm-li4' => 'data loss and outages can happen at any time (any complaints about that will likely be ignored)',
+
+    'agm-li5' => 'the db4free.net team is not granting any warranty or liability of any kind',
+
+    'agm-li6' => 'the db4free.net team reserves the right to delete databases and/or accounts at any time without notice',
+
+    'agm-li7' => 'it is up to you to get the latest information from <a href="/twitter.php">Twitter</a> and the <a href="/blog.php">db4free.net blog</a>',
+
+    'agm-li8' => 'db4free.net provides only a MySQL database, but no web space (there is nowhere to upload any files)',
+
+    'agm-p2' => 'Further:',
+
+    'agm-li9' => 'db4free.net is a service for testing, not for hosting. Databases that store more than 200 MB data will be cleared at irregular intervals without notification',
+
+    'agm-li10' => 'Please remove data which you no longer need, or <a href="/delete-account.php">delete your no longer needed account</a>. This makes it easier to recover if a server crash occurs.',
+
+    'signup-error1' => 'You have to agree with the conditions of use!',
+
+    'signup-error2' => 'Error registering the data for your MySQL database!',
+
+    'signup-error3' => 'Error sending the confirmation email!',
+
+    'signup-success' => 'Thank you for your registration! You will receive an email to confirm the registration.',
+);
+?>
---
+++ id/welcome.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Section: welcome
+Language: English
+*/
+
+$translations = array(
+    'h1' => 'Selamat datang di db4free.net',
+
+    'p1a' => 'db4free.net merupakan layanan tes untuk versi terbaru - terkadang versi pengembangan - dari <a href="https://www.mysql.com/">MySQL Server</a>. Anda dapat dengan mudah <a href="/signup.php">membuat akun dengan gratis</a> dan melakukan tes terhadap aplikasi Anda, contohnya Anda ingin mengetahui apa aplikasi Anda masih bekerja setelah MySQL update ke versi terbaru. db4free.net bagus untuk bahan edukasi dan membuat Anda terbiasa dengan fitur terbaru di versi MySQL yang terbaru.',
+
+    'p1b' => 'db4free.net bertujuan untuk selalu menyediakan rilisan terbaru versi produksi maupun pengembangan. MySQL server db4free.net\'s akan di-update secepatnya setelah versi terbaru dirilis, biasanya di hari yang sama.',
+
+    'p1c' => 'Untuk mengakses data Anda secara nyaman, db4free.net juga menyediakan sebuah versi terbaru dari <a href="http://www.phpmyadmin.net">phpMyAdmin</a>. phpMyAdmin juga akan selalu ter-update, jadi Anda akan selalu menggunakan versi terbarunya.',
+
+    'h2' => 'Apa db4free.net tidak baik untuk produksi',
+
+    'p2' => 'db4free.net adalah <em>layanan tes</em> yang berarti tidak baik untuk produksi. Di mana server dapat mati, atau Anda kehilangan data dan fitur keamanan tidak memenuhi standar yang Anda harapkan layaknya penyedia data hosting profesional. Jika Anda ingin menggunakan MySQL database untuk penggunaan produksi, harap tidak menggunakan db4free.net!',
+
+    'h3' => 'Apakah Anda dapat membantu menerjemahkan situs db4free.net?',
+
+    'p3' => 'Apa Anda berbicara lancar dengan bahasa yang tidak disediakan di situs ini? Anda dapat membantu kami menerjemahkan situs db4free.net. <a href="/translate.php">Lihat caranya!</a>',
+
+    'h4' => 'Sumber Lain',
+
+    'p4a' => 'Ada topik pembahasan <a href="https://www.mpopp.net/category/db4free/">db4free.net</a> di <a href="https://www.mpopp.net/">Blog mpopp.net</a> yang dapat memberikan Anda berita seputar db4free.net. Silakan subscribe di <a href="https://www.mpopp.net/category/db4free/feed/">RSS Feed</a> sehingga Anda tidak melewatkan berita terbarunya. db4free.net juga mempunyai <a href="/twitter.php">Twitter</a>, sumber lainnya yang dapat Anda lihat di tweets tentang db4free.net.',
+
+    'p4b' => 'Sumber paling bagus untuk belajar lebih lanjut tentang MySQL ada di <a href="https://dev.mysql.com/">MySQL Developer Zone</a>, juga <a href="https://dev.mysql.com/doc/refman/5.7/en/">MySQL Reference Manual</a> dan <a href="https://planet.mysql.com/">PlanetMySQL</a>. Situs MySQL memberikan segudang <a href="https://dev.mysql.com/tech-resources/articles/">Developer Articles</a> di mana banyak terdapat penjelasan tentang fitur terbaru yang akan diperkenalkan di versi yang akan datang selanjutnya dengan detil.',
+
+    'p4c' => 'Jika Anda menemukan <em>bug</em> di MySQL Server, silakan beritahu bug tersebut ke <a href="https://bugs.mysql.com/">MySQL Bug Tracking System</a>.',
+);
+?>
