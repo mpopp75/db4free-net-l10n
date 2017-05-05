43e49b5d965a4c70300bb8142347b4162e3596f2
Mr.Gagi <dragan1176@gmail.com>
2017-05-05 23:22:12 +0200

    Serbian translation

---
+++ sr/blog.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Section: blog
+Language: English
+*/
+
+$translations = array(
+    'h1' => 'mpopp.net blog',
+
+    'visitblog' => 'Posetite mpopp.net blog (otvara novi tab u pretraživaču)',
+
+    'listposts' => 'Lista blog postova:',
+);
+?>
