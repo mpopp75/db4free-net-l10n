38b952a7e993b3db1c07a918cbca38b2e552bbfe
Pawel Jagus <pawel.jagus@gmail.com>
2017-09-26 09:54:36 +0200

    A correction of typo "także" should be "tak, że". Also there were many repetitions of the word "także". Changed it to synonyms.

--- pl/welcome.php
+++ pl/welcome.php
@@ -11,11 +11,11 @@ $translations = array(
 
     'p1b' => 'db4free.net stara się zawsze zapewnić albo najnowszą wersję produktywną, albo najnowszą wersję deweloperską. Serwer MySQL na db4free.net jest aktualizowany tuż po wypuszczeniu nowej wersji, zwykle ma to miejsce tego samego dnia lub krótko potem.',
 
-    'p1c' => 'Aby zapewnić łatwy i wygodny dostęp do Twoich danych, db4free.net dostarcza także najnowszą wersję <a href="http://www.phpmyadmin.net">phpMyAdmin</a>. phpMyAdmin będzie także bardzo często aktualizowany, także zawsze będziesz miał dostęp do najnowszej wersji.',
+    'p1c' => 'Aby zapewnić łatwy i wygodny dostęp do Twoich danych, db4free.net dostarcza najnowszą wersję <a href="http://www.phpmyadmin.net">phpMyAdmin</a>. phpMyAdmin będzie ponadto bardzo często aktualizowany, tak, że zawsze będziesz miał dostęp do najnowszej wersji.',
 
     'h2' => 'Czym nie jest db4free.net',
 
-    'p2' => 'db4free.net to <b>serwis testowy</b>, co oznacza, że nie nadaje się i nie jest przeznaczony do celów produkcyjnych. System może być niedostępny przez nieokreślony czas, może dojść do utraty danych a także jego standardy bezpieczeństwa najpewniej nie odpowiadają Twoim wymaganiom, których oczekujesz od projesjonalnego dostawcy internetowego. Jeśli potrzebujesz bazy danych do celów produktywnych, prosimy, nie używaj db4free.net!',
+    'p2' => 'db4free.net to <b>serwis testowy</b>, co oznacza, że nie nadaje się i nie jest przeznaczony do celów produkcyjnych. System może być niedostępny przez nieokreślony czas, może dojść do utraty danych a dodatkowo jego standardy bezpieczeństwa najpewniej nie odpowiadają Twoim wymaganiom, których oczekujesz od projesjonalnego dostawcy internetowego. Jeśli potrzebujesz bazy danych do celów produktywnych, prosimy, nie używaj db4free.net!',
 
     'h3' => 'Czy masz czas i chęci przetłumaczyć db4free.net na język, który dobrze znasz?',
 
