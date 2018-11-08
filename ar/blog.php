dc574cd5c87e24d7abfffa6182e6114d20e07701
Markus Popp <git@mpopp.net>
2018-11-08 22:35:28 +0100

    Move Arabic translation files into ar directory

---
+++ ar/blog.php
@@ -0,0 +1,14 @@
+﻿<?php
+/*
+Section: المدونة
+Language: العربية
+*/
+
+$translations = array(
+    'h1' => 'mpopp.net مدونة',
+
+    'visitblog' => ' قم بزيارة المدونة  mpopp.net  (الفتح في علامة تبويب جديدة)',
+
+    'listposts' => 'عرض منشورات المدونة:',
+);
+?>
---
+++ ar/phpmyadmin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Section: phpmyadmin
+Language: ÇáÚÑÈíÉ
+*/
+
+$translations = array(
+    'h1' => 'phpMyAdmin',
+);
+?>
