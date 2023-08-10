dc051643d1b96aad9563d7266feafd6c6df3e515
Markus Popp <git@mpopp.net>
2023-08-10 06:28:23 +0200

    Remove some whitespace

--- pt/email-change-password.php
+++ pt/email-change-password.php
@@ -8,19 +8,20 @@ $translations = array(
     'email' => 'Recebemos sua solicitação de alteração de senha.
 
 Abaixo estão seus novos dados de acesso:
-    
+
 Usuário: {$user}
 Senha: {$newData}
-    
+
 Para confirmar as alterações, por favor, clique no link a seguir:
-    
+
 {$mailLink}
-    
+
 Caso não tenha solicitado uma mudança de senha para o seu banco de dados no db4free.net, por favor, desconsidere este e-mail.
 
 Esperamos que aproveite o seu banco de dados!
-    
+
 Atenciosamente,
-Equipe db4free.net',
+Equipe db4free.net
+https://www.db4free.net',
 );
 ?>
